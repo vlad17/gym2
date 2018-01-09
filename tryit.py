31ba1e767444a7ead9343a2e2d4544608c2e6479
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2018-01-08 16:02:25 -0800

    basic benchmarking

--- tryit.py
+++ tryit.py
@@ -1,19 +1,44 @@
 from contextlib import closing
 import sys
+import time
 
 import gym
 import numpy as np
-
-from gym2 import FullyObservableHalfCheetah
-
 import mujoco_py
-
-print(mujoco_py.get_version())
 
 
 def _usage():
     print('Usage: python tryit.py envname [render|bench]', file=sys.stderr)
     sys.exit(1)
+
+
+def _mjpv():
+    if hasattr(mujoco_py, 'get_version'):
+        return mujoco_py.get_version()
+    return '0.5.7'  # yeah, just assume
+
+
+def _env(envname):
+    if _mjpv() == '0.5.7':
+        envs = {
+            'hc': lambda: gym.make('HalfCheetah-v1')
+        }
+    else:
+        from gym2 import FullyObservableHalfCheetah
+        envs = {
+            'hc': FullyObservableHalfCheetah
+        }
+    if envname not in envs:
+        _usage()
+    return envs[envname]
+
+
+def is_gym2():
+    if _mjpv() == '0.5.7':
+        print('mujoco_py version {}, using old gym'.format(_mjpv()))
+        return False
+    print('mujoco_py version {}, using gym2'.format(_mjpv()))
+    return True
 
 
 def _random_actions(env, n):
@@ -28,20 +53,32 @@ def _random_actions(env, n):
     return acs
 
 
-def _render(envclass):
+def _render(envname):
     horiz = 100
+    envclass = _env(envname)
     with closing(envclass()) as env:
-        limited_env = gym.wrappers.TimeLimit(env, max_episode_steps=horiz)
+        if is_gym2():
+            env = gym.wrappers.TimeLimit(env, max_episode_steps=horiz)
         with closing(gym.wrappers.Monitor(
-                limited_env, 'render', force=True)) as rendered:
+                env, 'render', force=True)) as rendered:
             rendered.reset()
             for ac in _random_actions(rendered, horiz):
                 rendered.render()
                 rendered.step(ac)
 
 
-def _bench(envclass):
-    pass
+def _bench(envname):
+    is_gym2()
+    envclass = _env(envname)
+    with closing(envclass()) as env:
+        env.reset()
+        start = time.time()
+        for ac in _random_actions(env, 50000):
+            _, _, done, _ = env.step(ac)
+            if done:
+                env.reset()
+        end = time.time()
+        print('runtime for 50K steps {:.4g}'.format(end - start))
 
 
 def _main():
@@ -53,18 +90,12 @@ def _main():
         'render': True,
         'bench': False
     }
-    envs = {
-        'hc': FullyObservableHalfCheetah
-    }
-    if option not in should_render or envname not in envs:
-        _usage()
-
     np.random.seed(1234)
 
-    if should_render:
-        _render(envs[envname])
+    if should_render[option]:
+        _render(envname)
     else:
-        _bench(envs[envname])
+        _bench(envname)
 
 
 if __name__ == '__main__':
